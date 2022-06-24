eb9a7eec955662fd6e7faebd3ddf419b19bd0f9c
Frank Cano <frank.cano@utec.edu.pe>
2022-06-24 09:14:35 -0500

    ADD: user services

---
+++ github-stats/src/services/user-services.js
@@ -0,0 +1,30 @@
+import { tokenKey } from "../config";
+import githubClient from "./github-client";
+
+export async function createUser(userData) {
+  const { token, ...user } = await githubClient("/signup", { body: userData });
+  
+  sessionStorage.setItem(tokenKey, token);
+  return user;
+}
+
+export async function getUser() {
+  const { token, ...user } = await githubClient("/profile");
+  return user;
+}
+
+export async function updateUser(userData) {
+  const { _token, ...user } = await githubClient("profile", {
+    body: userData,
+    method: "PATCH"
+  });
+
+  return user;
+}
+
+export async function deleteUser() {
+  await githubClient("/profile", {method: "DELETE"});
+    sessionStorage.removeItem(tokenKey);
+}
+
+
