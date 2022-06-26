6f5ea81b81f4d50dac5d1a49e5308e53cc5237cc
Frank Cano <frank.cano@utec.edu.pe>
2022-06-25 20:19:50 -0500

    ADD: handle to delete favorites

--- github-stats/src/pages/search-page.js
+++ github-stats/src/pages/search-page.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import GithubUserData from '../components/githubUser-data';
 import Input from '../components/input';
+import { createFavorite, deleteFavorite } from '../services/favorites-services';
 import { getGithubUser } from '../services/githubapi-service';
-
 
 function SearchPage() {
   const [query, setQuery] = useState("");
+  const [favorites, setFavorites] = useState([])
   const [githubUser, setGithubUser] = useState(null);
+
+  const isFavorite = favorites.find((fav)=> fav.username === githubUser?.login);
+  console.log(isFavorite)
+  console.log(githubUser?.id)
 
   function handleSubmit(e) {
     e.preventDefault();
     getGithubUser(query)
         .then(setGithubUser)
         .catch(console.log);
-      }
- 
+  }
+
   function handleAddFavorite(e) {
     const data = {
         name: githubUser.name,
@@ -26,6 +31,21 @@ function SearchPage() {
                         .catch(console.log);
 
 
+  }
+
+  function handleRemovefavorite() {
+    const favorite = favorites.find((fav)=> fav.username === githubUser.login)
+    deleteFavorite(favorite.id).then(()=> {
+      const newFavorites = favorites.filter((fav)=> fav.username !== githubUser.login );
+      setFavorites(newFavorites)
+    })
+  }
+
+  useEffect(()=>{
+    console.log(favorites)
+    // console.log(isFavorite)
+  }, [favorites]);
+ 
   return (
     <form onSubmit={handleSubmit}>
       <Input 
@@ -37,7 +57,7 @@ function SearchPage() {
       <button>Search</button>
       <div>
         { !githubUser && "No users" }
-        { githubUser && !githubUser.message && <GithubUserData githubUser={githubUser}/>}
+        { githubUser && !githubUser.message && <GithubUserData githubUser={githubUser} onAddFavorite={handleAddFavorite} isFavorite={isFavorite} onDeleteFav={handleRemovefavorite}/>}
         { githubUser && githubUser.message && <p>{githubUser.message}</p> }
       </div>
     </form>
